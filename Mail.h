9f0b07b2a08a0954a7839582d27cfe5f6d3202e8
Atharva Chauhan <46600294+TAGISWILD@users.noreply.github.com>
2021-11-16 21:21:08 +0530

    Update Mail.h

--- Mail.h
+++ Mail.h
@@ -15,6 +15,7 @@ void Init(void)
 	printf("\n\nInitiating please wait\n");
 	fprintf(MainCRET,"import smtplib\nurm=open('GmailUR.txt','r')\nurp=open('PassUR.txt','r')\nsocmail = urm.read()\npassword = urp.read()\nsub = open('Subject.txt','r')\nsubject = sub.read()\nsandesh = open('Message.txt','r')\nsmessage = sandesh.read()\nmailid = open('Mail.txt','r')\nj = mailid.readline()\nEmail=j.split()\ns = smtplib.SMTP('smtp.gmail.com', 587)\ns.starttls()\ns.login(socmail, password)\nbody = ''\nding = 'Subject:{}{}'.format(subject, body)\nmessage = ding+smessage\n#print('Message Sent to',Email)\ns.sendmail(socmail, Email, smessage)\nprint('Done')\n\n");
 	fclose(MainCRET);
+	return 0;
 }
 void fillDat(void)
 {
